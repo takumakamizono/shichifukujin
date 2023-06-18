8f82b009b41f26c6db80988e5ec7828d7a2db504
神園巧真 <>
2023-06-18 11:52:03 +0900

    不要なファイルを削除しました

--- shichifukujin-theme/include/map.php
+++
@@ -1,5 +0,0 @@
-<div class="map">
-    <div class="map__inner">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4132.853144056339!2d130.75007407248057!3d31.770344720771952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x353efc5a23f65857%3A0x7b825b6a0a450d4!2z77yI5qCq77yJ5bed5Y6f5bu66Kit!5e0!3m2!1sja!2sjp!4v1679553162426!5m2!1sja!2sjp" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-</div>
