538fa2919ad4e6e454147004c08cf135f6e282ee
神園巧真 <>
2023-06-18 11:55:40 +0900

    トップページの会社概要のスタイル設定

--- shichifukujin-theme/include/company.php
+++ shichifukujin-theme/include/company.php
@@ -1,17 +1,26 @@
 <section class="company">
+<div class="section-titles">
+                  <h2 class="main-title">
+                  社会福祉法人天祐会とは
+                  </h2>
+                  <p class="sub-title tween-animate-title">
+                  company
+                  </p>
+                </div>
                 <div class="company__inner">
+                  <div class="company__overview">
                   <div class="company__img cover-slide">
                     <img class="img-zoom" src="https://placehold.jp/670x400.png" />
                   </div>
                   <div class="company__texts appear right">
                     <div class="company__texts-inner">
                       <div class="company__title main-title item">
-                        COMPANY
+                        会社紹介
                       </div>
-                      <div class="company__sub sub-title item">人とのつながりを大事に</div>
+                     
                       <div class="company__description item">
                         <p>
-                        地域の発展に寄与できる<br />企業を目指して
+                        七福神グループは日本一多く、その人の<br />「できたらいいなぁ」を実現します。
                         </p>
                        
                       </div>
@@ -20,6 +29,30 @@
                       
                       </div>
                     </div>
+                  </div>
+                  </div>
+                  <div class="company__institution">
+                  <div class="company__img cover-slide">
+                    <img class="img-zoom" src="https://placehold.jp/670x400.png" />
+                  </div>
+                  <div class="company__texts appear right">
+                    <div class="company__texts-inner">
+                      <div class="company__title main-title item">
+                        施設・事業紹介
+                      </div>
+                      <div class="company__description item">
+                      <p>
+                      特別養護老人ホームから就労支援センターまで<br />
+                        鹿児島市内5つのエリアに11の事業所を展開。<br />
+                        </p>
+                       
+                      </div>
+                      <div class="company__btn item">
+                        <a class="btn02" href="<?= esc_url(home_url('company')); ?>">more</a>
+                      
+                      </div>
+                    </div>
+                  </div>
                   </div>
                 </div>
               </section>
