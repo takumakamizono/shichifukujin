8bdc61ef658fb029b313689cdf2f42eed043b7bf
神園巧真 <>
2023-06-18 12:00:29 +0900

    よくある質問やスタッフインタビューのブロックの制作

---
+++ shichifukujin-theme/include/detailed-info.php
@@ -0,0 +1,70 @@
+
+<section class="detailed-info">
+    
+<div class="section-titles">
+                  <h2 class="main-title">
+                  社会福祉法人天祐会とは
+                  </h2>
+                  <p class="sub-title tween-animate-title">
+                  company
+                  </p>
+                </div>
+                <div class="detailed-info__inner">
+                    <div class="detailed-info__block">
+                    <div class="detailed-info__tit">
+                        ご利用の流れ
+                      </div>
+                        <div class="detailed-info__img">
+                            <img src="https://placehold.jp/670x400.png" alt="">
+                        </div>
+                        <div class="detailed-info__text">
+                            <div class="detailed-info__desc">
+                        <p>お客様から頂いたよくある質問をまとめました。</p>
+                        </div>
+                        <a class="btn slide-bg ">
+                            詳細はこちら
+                       </a>
+                       </div>
+
+                    </div>
+                    <div class="detailed-info__block">
+                    <div class="detailed-info__tit">
+                        スタッフインタビュー
+                      </div>
+                        <div class="detailed-info__img">
+                            <img src="https://placehold.jp/670x400.png" alt="">
+                        </div>
+                        <div class="detailed-info__text">
+                            <div class="detailed-info__desc">
+                        <p>お客様から頂いたよくある質問をまとめました。</p>
+
+                            </div>
+                        <a class="btn slide-bg ">
+                        詳細はこちら
+                       </a>
+                       </div>
+
+                    </div>
+                    <div class="detailed-info__block">
+                      <div class="detailed-info__tit">
+                        よくある質問
+                      </div>
+                        <div class="detailed-info__img">
+                            <img src="https://placehold.jp/670x400.png" alt="">
+                        </div>
+                        <div class="detailed-info__text">
+                            <div class="detailed-info__desc">
+                            <p>お客様から頂いたよくある質問をまとめました。</p>
+
+                            </div>
+
+                        <a class="btn slide-bg ">
+                        詳細はこちら
+                       </a>
+                       </div>
+
+                    </div>
+                </div>
+                <div class="detailed-info__bg"></div>
+ </section>
+
