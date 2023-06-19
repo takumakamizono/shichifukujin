10c697535f49c21ab0ca216f0d88e24cc445e8ec
神園巧真 <>
2023-06-19 16:54:46 +0900

    フッターの制作

--- shichifukujin-theme/include/footer.php
+++ shichifukujin-theme/include/footer.php
@@ -1,13 +1,22 @@
 <div class="footer appear up">
           <div class="footer__inner">
             <div class="footer__left">
-          <div class="logo item">
-             original theme
-            </div> 
+            <div class="logo item">
+            
+            <a href="<?= esc_url(home_url('/')); ?>">
+            <div class="logo__img">
+            <img src="<?= get_template_directory_uri(); ?>/images/logo.png" alt="">
+            </div>
+            <div class="logo__tit">
+              <span>社会福祉法人</span>
+           <span>天佑会</span> 
+           </div>
+           </a>
+            </div>
             <ul class="footer__address">
-              <li>〒000-1234　鹿児島県姶良市山田１−１</li>
-              <li>TEL : ０９９５−００−００００</li>
-              <li>FAX : ０９９５−１１−１１１１</li>
+              <li>〒890-0082　鹿児島市紫原５-１３-１８</li>
+              <li>TEL : ０９９−２５６−２７２９</li>
+              <li>FAX : ０９９−２５６−３７２９</li>
             </ul>    
           <nav class="footer__nav">
           <?php
@@ -31,7 +40,7 @@
        
           </div> 
         </div>
-        <div class="footer__copyright item">Copyright&copy; aomaru<span> All Rights Reserved.</span> </div>
+        <div class="footer__copyright item">Copyright&copy; 社会福祉法人天祐会.<span> All Rights Reserved.</span> </div>
         <div id="page_top"><a href="#"></a></div>
         </div>
         
