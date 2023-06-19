9fe2da94f4abb268e1a7ff5ca20056f613418471
神園巧真 <>
2023-06-19 16:59:10 +0900

    家族ページボタン部分のスタイル等の修正

--- shichifukujin-theme/include/family-page.php
+++ shichifukujin-theme/include/family-page.php
@@ -1,4 +1,12 @@
 <section class="family-page">
+<div class="section-titles">
+                  <h2 class="main-title">
+                  ご入居様ご家族ページ
+                  </h2>         
+                </div>
+        <a class="btn shadow02" href="">ご家族ページはコチラから</a>
+
+
     <div class="family-page__inner">
 
     </div>
