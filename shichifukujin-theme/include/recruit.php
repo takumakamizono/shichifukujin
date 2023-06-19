a04029eb0be88cb1de7dd7bb21e1c373b7dc25bc
神園巧真 <>
2023-06-19 16:47:07 +0900

    リクルートセクションの制作

---
+++ shichifukujin-theme/include/recruit.php
@@ -0,0 +1,29 @@
+<section class="recruit">
+<div class="section-titles">
+                  <h2 class="main-title">
+                  採用について
+                  </h2>    
+                  <p class="sub-title tween-animate-title">
+                  recruit
+                  </p>     
+                </div>
+      
+
+
+    <div class="recruit__inner">
+        <div class="recruit__img">
+            <img src="<?= get_template_directory_uri(); ?>/images/recruit.png" alt="">
+        </div>
+        <div class="recruit__texts">
+            <div class="recruit__texts-inner">
+       <p>私達は、ご利用者様にとっての理想的な住まいをつくり上げ、日々アップデートしていくために、新しい仲間を求めています。スタッフ間はとても仲が良く、
+        入社後の周りのサポートも優しく手厚い、あたたかい職場というのが自慢です。あなたも、私達と一緒にご利用者様の日常をつくり上げていきませんか？</p>
+        <div class="recruit__texts-btn">
+        <a class="btn solid" href="">採用について</a>
+        </div> 
+          </div>
+        </div>
+
+    </div>
+
+</section>
