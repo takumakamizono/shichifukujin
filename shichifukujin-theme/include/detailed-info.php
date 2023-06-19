3b2c88859117761848f3c3021d15ebd6fa9b1b8b
神園巧真 <>
2023-06-19 16:57:47 +0900

    よくある質問やインタビュー部分の文章の変更をしました。

--- shichifukujin-theme/include/detailed-info.php
+++ shichifukujin-theme/include/detailed-info.php
@@ -12,14 +12,14 @@
                 <div class="detailed-info__inner">
                     <div class="detailed-info__block">
                     <div class="detailed-info__tit">
-                        ご利用の流れ
+                        ご入居までの流れ
                       </div>
                         <div class="detailed-info__img">
                             <img src="https://placehold.jp/670x400.png" alt="">
                         </div>
                         <div class="detailed-info__text">
                             <div class="detailed-info__desc">
-                        <p>お客様から頂いたよくある質問をまとめました。</p>
+                        <p>ご入居をお考えの方へご利用までの流れをご説明します。</p>
                         </div>
                         <a class="btn slide-bg ">
                             詳細はこちら
@@ -36,7 +36,7 @@
                         </div>
                         <div class="detailed-info__text">
                             <div class="detailed-info__desc">
-                        <p>お客様から頂いたよくある質問をまとめました。</p>
+                        <p>各施設の七福神スタッフにインタビューしました。</p>
 
                             </div>
                         <a class="btn slide-bg ">
