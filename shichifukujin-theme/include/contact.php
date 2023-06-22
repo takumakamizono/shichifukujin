394dc7ef680bc18069a142a13e49dfe2ddf1c5a8
神園巧真 <>
2023-06-22 21:39:48 +0900

    トップページのお問い合わせボタンに矢印アイコンを設置

--- shichifukujin-theme/include/contact.php
+++ shichifukujin-theme/include/contact.php
@@ -7,7 +7,9 @@
         <h2 class="contact__title">お問い合わせ</h2>
         <p>ご入居・見学希望は下記からお気軽にお問い合わせください。</p>
         <div class="contact__btn">
-        <a class="btn shadow02" href="">お問い合わせフォーム</a>
+        <a class="btn shadow02" href="">
+        <i class="fa fa-envelope"></i>
+        お問い合わせフォーム</a>
         </div> 
 
    </div>
