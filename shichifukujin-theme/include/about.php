a551ebe6ce3b026ff39c0a67f9a275b2526de646
神園巧真 <>
2023-06-24 19:04:42 +0900

    トップの会社紹介セクションのclass名をcompanyからaboutに変更

--- shichifukujin-theme/include/about.php
+++ shichifukujin-theme/include/about.php
@@ -1,54 +1,54 @@
-<section class="company">
+<section class="about">
 <div class="section-titles">
                   <h2 class="main-title">
                   社会福祉法人天祐会とは
                   </h2>
                   <p class="sub-title tween-animate-title">
-                  company
+                  about
                   </p>
                 </div>
-                <div class="company__inner">
-                  <div class="company__overview">
-                  <div class="company__img cover-slide">
+                <div class="about__inner">
+                  <div class="about__overview">
+                  <div class="about__img cover-slide">
                     <img class="img-zoom" src="https://placehold.jp/670x400.png" />
                   </div>
-                  <div class="company__texts appear right">
-                    <div class="company__texts-inner">
-                      <div class="company__title main-title item">
+                  <div class="about__texts appear right">
+                    <div class="about__texts-inner">
+                      <div class="about__title main-title item">
                         会社紹介
                       </div>
                      
-                      <div class="company__description item">
+                      <div class="about__description item">
                         <p>
                         七福神グループは日本一多く、その人の<br />「できたらいいなぁ」を実現します。
                         </p>
                        
                       </div>
-                      <div class="company__btn item">
-                        <a class="btn02" href="<?= esc_url(home_url('company')); ?>">more</a>
+                      <div class="about__btn item">
+                        <a class="btn02" href="<?= esc_url(home_url('about')); ?>">会社紹介へ</a>
                       
                       </div>
                     </div>
                   </div>
                   </div>
-                  <div class="company__institution">
-                  <div class="company__img cover-slide">
+                  <div class="about__institution">
+                  <div class="about__img cover-slide">
                     <img class="img-zoom" src="https://placehold.jp/670x400.png" />
                   </div>
-                  <div class="company__texts appear right">
-                    <div class="company__texts-inner">
-                      <div class="company__title main-title item">
+                  <div class="about__texts appear right">
+                    <div class="about__texts-inner">
+                      <div class="about__title main-title item">
                         施設・事業紹介
                       </div>
-                      <div class="company__description item">
+                      <div class="about__description item">
                       <p>
                       特別養護老人ホームから就労支援センターまで<br />
                         鹿児島市内5つのエリアに11の事業所を展開。<br />
                         </p>
                        
                       </div>
-                      <div class="company__btn item">
-                        <a class="btn02" href="<?= esc_url(home_url('company')); ?>">more</a>
+                      <div class="about__btn item">
+                        <a class="btn02" href="http://localhost:4014/service">施設・事業紹介へ</a>
                       
                       </div>
                     </div>
