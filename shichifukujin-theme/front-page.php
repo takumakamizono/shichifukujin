390be85c28961b1854240ff8c01700c011d10d71
神園巧真 <>
2023-06-29 10:24:51 +0900

    トップページの読み込みの設定 comoanyでなくabout

--- shichifukujin-theme/front-page.php
+++ shichifukujin-theme/front-page.php
@@ -18,7 +18,7 @@
             <!-- <?php get_template_part('include/side-left'); ?> -->
             <main>
               <?php get_template_part('include/family-page'); ?>
-              <?php get_template_part('include/company'); ?>
+              <?php get_template_part('include/about'); ?>
               <?php get_template_part('include/detailed-info'); ?>
               <?php get_template_part('include/news'); ?>
               <?php get_template_part('include/recruit'); ?>
