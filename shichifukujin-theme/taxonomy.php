49db86395355567b51f7c98e9dc990df2808f298
神園巧真 <>
2023-06-29 10:07:07 +0900

    タクソノミーページの制作

---
+++ shichifukujin-theme/taxonomy.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+  <?php get_header();?>
+  </head>
+  <body>
+    <div id="global-container">
+      <div id="container">
+        <div class="mobile-menu__cover"></div>
+        <div class="nav-trigger"></div>
+        <header class="header">
+        <?php get_template_part('include/header'); ?>
+        </header>
+        <div id="content">
+        <?php get_template_part('include/hero'); ?>
+          <div id="main-content">
+           
+            <!-- <?php get_template_part('include/side-left'); ?> -->
+            <main>
+            <section class="service">
+            <?php get_template_part('include/breadcrumb'); ?> 
+             <?php $facility =  get_queried_object(); ?>
+                <div class="section-titles">
+                  <h2 class="main-title">
+                    <span>
+                    <?php if ($facility) {
+                      echo esc_html ($facility->name);
+                     }  ?>
+                  </span>
+                  </h2>       
+                </div>
+             
+                <div class="service__inner">
+             <?php if(have_posts()): ?>   
+              <?php while(have_posts()):the_post(); ?>
+                  <?php get_template_part('include/service-inside'); ?>
+                  <?php endwhile; ?>
+           <?php else: ?>
+
+<div class="service__notInfo">
+                      <p>現在、準備中です</p>
+                    </div>
+
+            <?php endif; ?>
+
+              
+                </div>
+              </section>   
+        
+              <?php get_template_part('include/contact'); ?>
+            </main>
+            <!-- <?php get_template_part('include/side-right'); ?> -->
+          
+          </div>
+        </div>
+        <?php get_template_part('include/footer'); ?>
+  
+      </div>
+      <?php get_template_part('include/mobile-menu');?>
+    </div>
+    <?php wp_footer(); ?>
+  </body>
+</html>
