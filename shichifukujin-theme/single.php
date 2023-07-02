453675fa3c4d64983d260458c402165931c59519
神園巧真 <>
2023-07-02 09:59:54 +0900

    投稿の表示の設定に関してinterviewのカテゴリの時は違うテンプレートが表示されるように条件分岐で設定しました。

--- shichifukujin-theme/single.php
+++ shichifukujin-theme/single.php
@@ -23,25 +23,12 @@
                 <?php if(have_posts()): ?>
                   <?php while(have_posts()):the_post(); ?> 
                   <article id="post-<?php the_ID(); ?>" <?php post_class('single__content') ?> >
-                    <div class="single__content-header">
-                    <time><?php the_time( get_option( 'date_format' ) ); ?></time>  
-                    
-                     <h2><?php the_title(); ?></h2>         
-                    </div>
-                    <div class="single__content-des">
-                    <div class="single__content-des-img">
-                      <?php if(the_post_thumbnail()): ?>
-                      <?php the_post_thumbnail('medium'); ?>
-                        <?php endif; ?>
-                        </div>
-                        <div class="single__content-text">
-                      <?php the_content(); ?>
-                       </div>
-                    </div>
-                    <div class="postLinks">
-                      <div class="postLinks__link postLinks__link-prev"><?php previous_post_link('戻る%link','%title',true,'') ?></div>
-                      <div class="postLinks__link postLinks__link-next"><?php next_post_link('次へ%link','%title',true,'') ?></div>
-                    </div>
+                <?php if(in_category("interview")): ?>
+		             <?php get_template_part( "include/interview"); ?>
+             <?php else: ?>
+              <?php get_template_part( 'include/news-contents' ); ?>
+           <?php endif; ?>
+                
                   </article >
                   <?php endwhile; ?>   
                 <?php endif; ?> 
