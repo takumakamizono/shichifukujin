a9a11824bda37c8ca718d3369f09fec75a29ed20
神園巧真 <>
2023-07-02 09:35:20 +0900

    施設紹介一覧のボタンをコンテンツがある場合にのみ表示するように設定

--- shichifukujin-theme/include/service-inside.php
+++ shichifukujin-theme/include/service-inside.php
@@ -1,6 +1,4 @@
 <div id="post-<?php the_ID(); ?>" class="service__item">
-
-
 <div class="service__img">
                       <?php if(has_post_thumbnail()): ?>
                      <?php the_post_thumbnail('full'); ?>    
@@ -14,12 +12,15 @@
                     <p class="service__subtitle"><?php the_field('kind'); ?></p>
                     <p class="service__title"><?php the_title(); ?></p>
                 </div>
+                <?php //if ( get_field('btn_onoff') ): ?>
+                <div class="service__btn">
+                    <a class="btn shadow" href="<?php the_permalink(); ?>">  施設詳細はこちら</a>
+                    </div>
+                  <?php // endif; ?> 
                     <div class="service__desc">
                       <p><?php the_field('top_comment'); ?></p>
                     </div>
-                    <div class="service__btn">
-                    <a class="btn shadow" href="<?php the_permalink(); ?>">  施設詳細はこちら</a>
-                    </div>
+                   
                     </div>
                  
           
