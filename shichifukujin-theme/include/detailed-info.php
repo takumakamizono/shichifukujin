70ca6f6dbbdcd21ff385af3af49a1e3bcf68c1ee
神園巧真 <>
2023-07-02 10:14:06 +0900

    front-pageのご利用の流れなどのセクションのページリンクを設定

--- shichifukujin-theme/include/detailed-info.php
+++ shichifukujin-theme/include/detailed-info.php
@@ -12,7 +12,7 @@
                             <div class="detailed-info__desc">
                         <p>ご入居をお考えの方へご利用までの流れをご説明します。</p>
                         </div>
-                        <a class="btn slide-bg ">
+                        <a class="btn slide-bg" href="<?= home_url('/procedure/') ?>">
                             詳細はこちら
                        </a>
                        </div>
@@ -30,7 +30,11 @@
                         <p>各施設の七福神スタッフにインタビューしました。</p>
 
                             </div>
-                        <a class="btn slide-bg ">
+                           <?php
+                            $interview  = get_term_by('slug','interview','category');
+                            $interview_link = get_term_link($interview,'category');
+                           ?>
+                        <a class="btn slide-bg" href="<?= esc_url($interview_link); ?>" >
                         詳細はこちら
                        </a>
                        </div>
@@ -49,7 +53,7 @@
 
                             </div>
 
-                        <a class="btn slide-bg ">
+                        <a class="btn slide-bg" href="<?= home_url('/faq/') ?>">
                         詳細はこちら
                        </a>
                        </div>
