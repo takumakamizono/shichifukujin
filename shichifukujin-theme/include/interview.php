8597cfdecebbc0befa6babf55a464036e19706b3
神園巧真 <>
2023-07-04 14:31:07 +0900

    スタッフインタビューページの作成

---
+++ shichifukujin-theme/include/interview.php
@@ -0,0 +1,108 @@
+<section class="interview">
+<div class="section-titles">
+                  <h2 class="main-title">
+                    <span><?php the_title(); ?></span>    
+                    さんにインタビューしました。         
+                  </h2>                    
+                </div>
+    <div class="interview__inner">
+        <div class="interview__content">
+        <div class="interview__content-inner">
+        <div class="interview__img">
+        <?php if( get_field('img01')): ?>
+            <img src="<?php the_field('img01'); ?>" alt="">
+            <?php else: ?>
+            <img src="<?= get_template_directory_uri(); ?>/images/recruit.png" alt="">
+            <?php endif; ?>
+
+        </div>
+        <div class="interview__texts">          
+            <div class="interview__texts-inner">
+            <div class="interview__texts-desc">
+                <h3 class="interview__textsTit"><?php the_field('question01'); ?></h3>
+       <p><?php the_field('answer01'); ?></p>
+          </div>
+          </div>
+          </div>
+        </div>
+        </div>
+        <div class="interview__content">
+        <div class="interview__texts-desc">
+<h3 class="interview__textsTit"><?php the_field('question02'); ?></h3>
+<p><?php the_field('answer02'); ?></p>
+</div>
+       </div>
+       <div class="interview__content">
+        <div class="interview__content-inner">
+        <div class="interview__img">
+            
+        <?php if( get_field('img02')): ?>
+            <img src="<?php the_field('img02'); ?>" alt="">
+            <?php else: ?>
+            <img src="<?= get_template_directory_uri(); ?>/images/recruit.png" alt="">
+            <?php endif; ?>
+        </div>
+        <div class="interview__texts">          
+            <div class="interview__texts-inner">
+                <div class="interview__texts-desc">
+
+                <h3 class="interview__textsTit"><?php the_field('question03'); ?></h3>
+       <p><?php the_field('answer03'); ?></p>
+       </div>
+          </div>
+          </div>
+        </div>
+        </div>
+        <div class="interview__content">
+        <div class="interview__texts-desc">
+<h3 class="interview__textsTit"><?php the_field('question04'); ?></h3>
+<p><?php the_field('answer04'); ?></p>
+</div>
+       </div> 
+        <div class="interview__content">
+        <div class="interview__content-inner">
+        <div class="interview__img">
+        <?php if( get_field('img03')): ?>
+            <img src="<?php the_field('img03'); ?>" alt="">
+            <?php else: ?>
+            <img src="<?= get_template_directory_uri(); ?>/images/recruit.png" alt="">
+            <?php endif; ?>
+        </div>
+        <div class="interview__texts">          
+            <div class="interview__texts-inner">
+            <?php if( get_field('question05')): ?>
+                <div class="interview__texts-desc">           
+                <h3 class="interview__textsTit"> <?php the_field('question05'); ?></h3>          
+       <p><?php the_field('answer05'); ?></p>
+       </div>
+       <?php endif; ?>
+            <?php if( get_field('question06')): ?>
+                <div class="interview__texts-desc">           
+                <h3 class="interview__textsTit"> <?php the_field('question06'); ?></h3>          
+       <p><?php the_field('answer06'); ?></p>
+       </div>
+       <?php endif; ?>
+       <?php if( get_field('question07')): ?>
+                <div class="interview__texts-desc">
+
+                <h3 class="interview__textsTit"> <?php the_field('question07'); ?></h3>
+       <p><?php the_field('answer07'); ?></p>
+       </div>
+       <?php endif; ?>
+       <?php if( get_field('question08')): ?>
+                <div class="interview__texts-desc">
+
+                <h3 class="interview__textsTit"><?php the_field('question08'); ?></h3>
+       <p><?php the_field('answer08'); ?></p>
+       </div>
+       <?php endif; ?>
+          </div>
+          </div>
+        </div>
+        </div>
+
+
+
+    </div>
+
+</section>
