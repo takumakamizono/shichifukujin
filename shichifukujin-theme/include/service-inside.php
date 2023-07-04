e29405751246428122e629c2cd00e8513d26f8d0
神園巧真 <>
2023-07-04 14:40:44 +0900

    施設紹介一覧ページで特定の施設情報ページに飛ぶページリンクボタンを押せなくする設定

--- shichifukujin-theme/include/service-inside.php
+++ shichifukujin-theme/include/service-inside.php
@@ -10,13 +10,21 @@
             <div class="service__contents">
               <div class="service__titles">
                     <p class="service__subtitle"><?php the_field('kind'); ?></p>
-                    <p class="service__title"><?php the_title(); ?></p>
+                    <p class="service__title"><?php the_title(); ?></p>              
                 </div>
-                <?php //if ( get_field('btn_onoff') ): ?>
+                <div class="service__location">
+                  <?php the_field('location');?>
+                </div>
+               
                 <div class="service__btn">
-                    <a class="btn shadow" href="<?php the_permalink(); ?>">  施設詳細はこちら</a>
+                <?php if (get_field('onoff') ): ?>
+                  <a class="btn btn-off">施設詳細はこちら</a>
+                <?php  else: ?> 
+                  <a class="btn shadow" href="<?php the_permalink(); ?>">施設詳細はこちら</a>
+                  <?php endif;?>
                     </div>
-                  <?php // endif; ?> 
+                   
+              
                     <div class="service__desc">
                       <p><?php the_field('top_comment'); ?></p>
                     </div>
