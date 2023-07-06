5d452b5f7da2c63c5c046239e1ab3c55e5a43b63
神園巧真 <>
2023-07-06 10:48:44 +0900

    画像付きのコンテンツのパーツを制作

---
+++ shichifukujin-theme/service_inc/sec-cardinfo.php
@@ -0,0 +1,86 @@
+<div class="cardinfo">
+ <div class="section-titles">
+   <h3 class="main-title"><span><?php the_field('section-title'); ?></span></h3>            
+ </div>
+    <div class="cardinfo__inner">
+    <div class="cardinfo__blocks">
+        <div class="cardinfo__block">
+            <div class="cardinfo__img">
+         
+                    <img src="<?php the_field('cardinfo-img01'); ?>" alt="">
+              
+            </div>
+            <div class="cardinfo__txt">
+                <h5><?php the_field('cardinfo-title01'); ?></h5>
+           
+            </div>
+        </div>
+        <div class="cardinfo__block">
+            <div class="cardinfo__img">
+              
+                <img src="<?php the_field('cardinfo-img02'); ?>" alt="">       
+              
+            </div>
+            <div class="cardinfo__txt">
+                <h5><?php the_field('cardinfo-title02'); ?></h5>
+        
+            </div>
+        </div>
+        <div class="cardinfo__block">
+            <div class="cardinfo__img">
+           
+                <img src="<?php the_field('cardinfo-img03'); ?>" alt="">       
+              
+            </div>
+            <div class="cardinfo__txt">
+                <h5><?php the_field('cardinfo-title03'); ?></h5>
+           
+            </div>
+        </div>
+        <?php if(get_field('cardinfo-title04')): ?>
+        <div class="cardinfo__block">
+            <div class="cardinfo__img">
+             
+                <img src="<?php the_field('cardinfo-img04'); ?>" alt="">       
+           
+              
+            </div>
+
+            <div class="cardinfo__txt">
+                <h5><?php the_field('cardinfo-title04'); ?></h5>
+            </div>
+        </div>
+        <?php  endif;?>
+        <?php if(get_field('cardinfo-title05')): ?>
+        <div class="cardinfo__block">
+            <div class="cardinfo__img">
+               
+                    <img src="<?php the_field('cardinfo-img05'); ?>" alt="">               
+             
+            </div>
+            <div class="cardinfo__txt">
+                <h5><?php the_field('cardinfo-title05'); ?></h5>
+       
+            </div>
+        </div>
+        <?php endif; ?>
+        <?php if(get_field('cardinfo-title06')): ?>
+        <div class="cardinfo__block">
+            <div class="cardinfo__img">
+          
+                    <img src="<?php the_field('cardinfo-img06'); ?>" alt="">              
+            
+            </div>
+            <div class="cardinfo__txt">
+                <h5><?php the_field('cardinfo-title06'); ?></h5>
+     
+            </div>
+        </div>
+        <?php endif; ?>
+    </div>
+    <div class="cardinfo__desc">
+        <p><?php the_field('cardinfo-desc'); ?></p>
+    </div>
+    </div>
+
+ </div>
