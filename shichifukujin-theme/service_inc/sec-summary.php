5df5e4a502a0a22611e6d0884f9bb6f4d891b614
神園巧真 <>
2023-07-06 10:49:33 +0900

    施設概要の一覧の制作

--- shichifukujin-theme/service_inc/sec-summary.php
+++ shichifukujin-theme/service_inc/sec-summary.php
@@ -1,50 +1,61 @@
-<div class="sl-service__sec03">
+<div class="surround-contents">
     
    
-   <div class="sl-service__sec03-inner">
-    <div class="sl-service__sec03-box">
+   <div class="surround-contents__inner">
+    <div class="surround-contents__box">
     <div class="content-title">
     <h4 class="main-title">施設概要</h4>
   </div>
  
 <table class="table01">
 <tr>
-<th>施設名称</th>
-<td><?php the_field('overview01');?></td>
+<th><?php the_field('summary-tit01');?></th>
+<td><?php the_field('summary-desc01');?></td>
 </tr>
 <tr>
-<th>施設の種類</th>
-<td><?php the_field('overview02');?></td>
+<th><?php the_field('summary-tit02');?></th>
+<td><?php the_field('summary-desc02');?></td>
 </tr>
 <tr>
-<th>住所</th>
-<td><?php the_field('overview03');?></td>
+<th><?php the_field('summary-tit03');?></th>
+<td><?php the_field('summary-desc03');?></td>
 </tr>
+<?php if(get_field('summary-tit04')): ?>
 <tr>
-<th>TEL</th>
-<td><?php the_field('overview04');?></td>
+<th><?php the_field('summary-tit04');?></th>
+<td><?php the_field('summary-desc04');?></td>
 </tr>
+<?php endif; ?>
+<?php if(get_field('summary-tit05')): ?>
 <tr>
-<th>FAX</th>
-<td><?php the_field('overview05');?></td>
+<th><?php the_field('summary-tit05');?></th>
+<td><?php the_field('summary-desc05');?></td>
 </tr>
+<?php endif; ?>
+<?php if(get_field('summary-tit06')): ?>
 <tr>
-<th>共同施設</th>
-<td><?php the_field('overview06');?></td>
+<th><?php the_field('summary-tit06');?></th>
+<td><?php the_field('summary-desc06');?></td>
 </tr>
+<?php endif; ?>
+<?php if(get_field('summary-tit07')): ?>
 <tr>
-<th>交通</th>
-<td><?php the_field('overview07');?></td>
+<th><?php the_field('summary-tit07');?></th>
+<td><?php the_field('summary-desc07');?></td>
 </tr>
+<?php endif; ?>
+<?php if(get_field('summary-tit08')): ?>
 <tr>
-<th>建物完成</th>
-<td><?php the_field('overview08');?></td>
+<th><?php the_field('summary-tit08');?></th>
+<td><?php the_field('summary-desc08');?></td>
 </tr>
+<?php endif; ?>
+<?php if(get_field('summary-tit09')): ?>
 <tr>
-<th>入居時期</th>
-<td><?php the_field('overview09');?></td>
+<th><?php the_field('summary-tit09');?></th>
+<td><?php the_field('summary-descw09');?></td>
 </tr>
+<?php endif; ?>
 </table>
-  </div>
-         
+  </div>  
  </div>
