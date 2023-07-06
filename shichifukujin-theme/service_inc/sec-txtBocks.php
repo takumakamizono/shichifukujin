ef9a8f58d4ee14cd0f0547ab62815c81fa061c51
神園巧真 <>
2023-07-06 10:31:01 +0900

    sec2のコンテンツをパーツに分離しインクルードする設定とカスタムフィールドの設定

--- shichifukujin-theme/service_inc/sec-txtBocks.php
+++ shichifukujin-theme/service_inc/sec-txtBocks.php
@@ -2,11 +2,10 @@
                     <div class="texts-blocks__inner">
                       <div class="texts-blocks__box">
                       <h4 class="texts-blocks__title">
-                        <span>ケアの内容</span>
+                        <span><?php the_field('text-title01');?></span>
                      </h4>
                      <div class="texts-blocks__desc">
-                      <p>9名の認知症の利用者様が一緒に生活しています。部屋はそれぞれ個室ですが、9名は家族のように、リビングで過ごす時間を大切にしています。施設のタイムスケジュールに利用者様を合わせるのではなく、職員が一人一人の利用者様の生活リズムに合わせて1日の暮らしを創っています。
-                       <br>また、食事介助や排泄介助はもちろんですが、利用者様の「できたらいいなぁ」を叶えるお手伝いも致します。</p>
+                      <p><?php the_field('text-desc01');?></p>
                      </div>
                       </div>
                       </div>
