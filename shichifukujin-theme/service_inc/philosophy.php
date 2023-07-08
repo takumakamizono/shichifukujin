a175c1ad3a3778c17fba3342d53b9b5c940764f6
神園巧真 <>
2023-07-08 09:54:07 +0900

    就労支援センターの施設理念セクションの制作

---
+++ shichifukujin-theme/service_inc/philosophy.php
@@ -0,0 +1,47 @@
+<div class="txtimg-block">
+<div class="section-titles">
+   <h3 class="main-title"><span>施設理念</span></h3>            
+ </div>
+    <div class="txtimg-block__inner">  
+    <div class="txtimg-block__box">
+                      <div class="txtimg-block__texts">
+                        <div class="txtimg-block__texts-inner">
+                        <h4 class="txtimg-block__title"> <?php the_field('philosophy-title01'); ?></h4>
+                          <div class="txtimg-block__desc">                                          
+                            <?php the_field('philosophy-desc01'); ?>                        
+                          </div>
+                        </div>  
+                      </div>
+                      <div class="txtimg-block__img">
+                    <img src="<?php the_field('philosophy-img01');  ?>" alt="">  
+                      </div>          
+                    </div>
+                    <div class="txtimg-block__box">
+                      <div class="txtimg-block__texts">
+                        <div class="txtimg-block__texts-inner">
+                        <h4 class="txtimg-block__title"> <?php the_field('philosophy-title02'); ?></h4>
+                          <div class="txtimg-block__desc">                                          
+                            <?php the_field('philosophy-desc02'); ?>                        
+                          </div>
+                        </div>  
+                      </div>
+                      <div class="txtimg-block__img">
+                    <img src="<?php the_field('philosophy-img02');  ?>" alt="">  
+                      </div>          
+                    </div> <div class="txtimg-block__box">
+                      <div class="txtimg-block__texts">
+                        <div class="txtimg-block__texts-inner">
+                        <h4 class="txtimg-block__title"> <?php the_field('philosophy-title03'); ?></h4>
+                          <div class="txtimg-block__desc">                                          
+                            <?php the_field('philosophy-desc03'); ?>                        
+                          </div>
+                        </div>  
+                      </div>
+                      <div class="txtimg-block__img">
+                    <img src="<?php the_field('philosophy-img03');  ?>" alt="">  
+                      </div>          
+                    </div>
+              </div>
+                </div>
+            </div>
+               
