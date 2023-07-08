8f6c42326756916fac072b525bf66d7eaba3515f
神園巧真 <>
2023-07-08 09:40:07 +0900

    資料請求のバナーセクションの制作

---
+++ shichifukujin-theme/service_inc/banner.php
@@ -0,0 +1,16 @@
+<section class="banner">
+    <div class="banner__inner">
+    <h2 class="banner__title">資料請求</h2>
+    <div class="banner__texts">
+       
+        <p>医療・福祉・介護サービスとすみやかに連携を図り必要に応じた相談対応や提案等の支援を行います。</p>
+        <div class="banner__btn">
+        <a class="btn shadow" href="">
+        <i class="fa fa-envelope"></i>
+        資料請求</a>
+        </div> 
+    
+   </div>
+    </div>
+
+</section>
