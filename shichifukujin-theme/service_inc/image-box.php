46dd50e5e0c5d454ecff5e976120732ed36c0c18
神園巧真 <>
2023-07-08 09:55:52 +0900

    画像一覧のテンプレートパーツを制作

---
+++ shichifukujin-theme/service_inc/image-box.php
@@ -0,0 +1,35 @@
+<?php if(get_field('img01')): ?>
+<div class="image-box">
+<?php if(get_field('img01')): ?>
+                        <div class="image-box__img">
+                          <img src="<?php the_field('img01');?>" alt="">
+                          <p><?php the_field('image-title01');?></p>
+                        </div>
+                        <?php endif; ?>
+                        <?php if(get_field('img02')): ?>
+                        <div class="image-box__img">
+                          <img src="<?php the_field('img02');?>" alt="">
+                          <p><?php the_field('image-title02');?></p>
+                        </div>
+                        <?php endif; ?>
+                        <?php if(get_field('img03')): ?>
+                        <div class="image-box__img">
+                          <img src="<?php the_field('img03');?>" alt="">
+                          <p><?php the_field('image-title03');?></p>
+                       </div>
+                       <?php endif; ?>
+                        <?php if(get_field('img04')): ?>
+                        <div class="image-box__img">
+                          <img src="<?php the_field('img04');?>" alt="">
+                          <p><?php the_field('image-title04');?></p>
+                       </div>
+                       <?php endif; ?>
+                        <?php if(get_field('img05')): ?>
+                        <div class="image-box__img">
+                          <img src="<?php the_field('img05');?>" alt="">
+                          <p><?php the_field('image-title05');?></p>
+                       </div>
+                       <?php endif; ?>
+
+                      </div>
+                      <?php endif; ?>
