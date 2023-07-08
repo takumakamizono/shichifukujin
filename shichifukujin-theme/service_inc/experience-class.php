68e158ed65758aaad261e84070125552477b86d2
神園巧真 <>
2023-07-08 09:50:53 +0900

    デイサービスセンターページの無料体験教室のセクションの制作。
    右に画像、左にテキストがくる２カラムのスタイル

---
+++ shichifukujin-theme/service_inc/experience-class.php
@@ -0,0 +1,22 @@
+<div class="txtimg-block">
+<div class="section-titles">
+   <h3 class="main-title"><span>無料体操教室</span></h3>            
+ </div>
+    <div class="txtimg-block__inner">
+    <div class="txtimg-block__box">
+                      <div class="txtimg-block__texts">
+                        <div class="txtimg-block__texts-inner">
+                          <div class="txtimg-block__desc">                                          
+                            <?php the_field('classroom-comment'); ?>                        
+                          </div>
+                        </div>  
+                      </div>
+                      <div class="txtimg-block__img">
+                    <img src="<?php the_field('classroom-img');  ?>" alt="">  
+                      </div>          
+                    </div>
+      
+                </div>
+                </div>
+                </div>
+               
