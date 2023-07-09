9752449e4c6e066816204ed8097eb8ac5d860eaf
神園巧真 <>
2023-07-09 22:59:39 +0900

    heroスライダーのカスタムフィールドの設定

--- shichifukujin-theme/include/hero.php
+++ shichifukujin-theme/include/hero.php
@@ -6,8 +6,8 @@
                   <div class="hero__title">
                   <img class="hero__title-img" src="<?= get_template_directory_uri(); ?>/images/hero_title.png" alt="">
                   </div>
-                <?php if(get_field('top_img01',222)): ?>      
-              <img src="<?php the_field('top_img01',222);?>" alt="スライド画像01">
+                <?php if(get_field('top_img01',310)): ?>      
+              <img src="<?php the_field('top_img01',310);?>" alt="スライド画像01">
               <?php else: ?>
                   <img src="https://placehold.jp/1400x930.png" alt="トップスライド画像" />
               <?php endif; ?>
@@ -16,8 +16,8 @@
                   <div class="hero__title">
                   <img class="hero__title-img" src="<?= get_template_directory_uri(); ?>/images/hero_title.png" alt="">
                   </div>
-                  <?php if(get_field('top_img02',222)): ?>      
-              <img src="<?php the_field('top_img02',222);?>" alt="スライド画像02">
+                  <?php if(get_field('top_img02',310)): ?>      
+              <img src="<?php the_field('top_img02',310);?>" alt="スライド画像02">
               <?php else: ?>
                   <img src="https://placehold.jp/1400x930.png" alt="トップスライド画像" />
               <?php endif; ?>
@@ -27,8 +27,8 @@
                   <img class="hero__title-img" src="<?= get_template_directory_uri(); ?>/images/hero_title.png" alt="">
                   </div>
                  
-                  <?php if(get_field('top_img03',222)): ?>      
-              <img src="<?php the_field('top_img03',222);?>" alt="スライド画像03">
+                  <?php if(get_field('top_img03',310)): ?>      
+              <img src="<?php the_field('top_img03',310);?>" alt="スライド画像03">
               <?php else: ?>
                   <img src="https://placehold.jp/1400x930.png" alt="トップスライド画像" />
               <?php endif; ?>
