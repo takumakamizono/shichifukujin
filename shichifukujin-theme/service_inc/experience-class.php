189ac89f892533e9eb9de5dbdc64d439719f3eca
神園巧真 <>
2023-07-13 23:42:00 +0900

    デイサービスセンターの無料体験教室で使用するパーツの修正とカスタム投稿の設定、スタイル設定

--- shichifukujin-theme/service_inc/experience-class.php
+++ shichifukujin-theme/service_inc/experience-class.php
@@ -4,6 +4,7 @@
  </div>
     <div class="txtimg-block__inner">
     <div class="txtimg-block__box">
+      <div class="txtimg-block__box-inner">
                       <div class="txtimg-block__texts">
                         <div class="txtimg-block__texts-inner">
                           <div class="txtimg-block__desc">                                          
@@ -15,7 +16,15 @@
                     <img src="<?php the_field('classroom-img');  ?>" alt="">  
                       </div>          
                     </div>
+                    <div class="txtimg-block__bottom">
+                      <p class="txtimg-block__bottom-tit">開催日程・開催場所</p>
+                      <div class="txtimg-block__bottom-desc">
+                    <?php the_field('schedule-desc');  ?>
+                    </div>
       
+      </div>
+       </div>
+              
                 </div>
                 </div>
                 </div>
