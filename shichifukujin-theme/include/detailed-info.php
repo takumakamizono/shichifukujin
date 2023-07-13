95fef6acc027def7c5c0df256b39777024fb23a3
神園巧真 <>
2023-07-13 23:20:16 +0900

    スタッフインタビューやよくある質問部分の画像の設定

--- shichifukujin-theme/include/detailed-info.php
+++ shichifukujin-theme/include/detailed-info.php
@@ -6,15 +6,17 @@
                         ご入居までの流れ
                       </div>
                         <div class="detailed-info__img">
-                            <img src="https://placehold.jp/670x400.png" alt="">
+                            <img src="<?= get_template_directory_uri(); ?>/images/670×400_01.png" alt="">
                         </div>
                         <div class="detailed-info__text">
                             <div class="detailed-info__desc">
                         <p>ご入居をお考えの方へご利用までの流れをご説明します。</p>
                         </div>
+                        <div class="detailed-info__btn">
                         <a class="btn slide-bg" href="<?= home_url('/procedure/') ?>">
                             詳細はこちら
                        </a>
+                       </div>
                        </div>
 
                     </div>
@@ -23,7 +25,7 @@
                         スタッフインタビュー
                       </div>
                         <div class="detailed-info__img">
-                            <img src="https://placehold.jp/670x400.png" alt="">
+                            <img src="<?= get_template_directory_uri(); ?>/images/670×400_02.png" alt="">
                         </div>
                         <div class="detailed-info__text">
                             <div class="detailed-info__desc">
@@ -34,9 +36,11 @@
                             $interview  = get_term_by('slug','interview','category');
                             $interview_link = get_term_link($interview,'category');
                            ?>
+                    <div class="detailed-info__btn">
                         <a class="btn slide-bg" href="<?= esc_url($interview_link); ?>" >
                         詳細はこちら
                        </a>
+                       </div>
                        </div>
 
                     </div>
@@ -45,17 +49,18 @@
                         よくある質問
                       </div>
                         <div class="detailed-info__img">
-                            <img src="https://placehold.jp/670x400.png" alt="">
+                            <img src="<?= get_template_directory_uri(); ?>/images/670×400_03.png" alt="">
                         </div>
                         <div class="detailed-info__text">
                             <div class="detailed-info__desc">
                             <p>お客様から頂いたよくある質問をまとめました。</p>
 
                             </div>
-
+                            <div class="detailed-info__btn">
                         <a class="btn slide-bg" href="<?= home_url('/faq/') ?>">
                         詳細はこちら
                        </a>
+                       </div>
                        </div>
 
                     </div>
