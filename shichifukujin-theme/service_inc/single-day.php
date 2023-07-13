c5774d7fb61d322e084b5f33d9e2da96eb8c608b
神園巧真 <>
2023-07-13 23:45:34 +0900

    ララシャンスページにも5つの取り組みセクションを表示できるように設定

--- shichifukujin-theme/service_inc/single-day.php
+++ shichifukujin-theme/service_inc/single-day.php
@@ -10,12 +10,12 @@
 <?php if($post_slug === 'chao'): ?>
 <?php get_template_part('service_inc/sec-cardinfo'); ?>
 <?php endif; ?>
-<?php if($post_slug === 'yokana'||$post_slug === 'servicecenter'): ?>
+<?php if($post_slug === 'yokana'||$post_slug === 'servicecenter'||$post_slug === 'larashouse'): ?>
  <?php get_template_part('service_inc/sec-try'); ?>
  <?php endif; ?>
  <?php if($post_slug === 'servicecenter'): ?>
  <?php get_template_part('service_inc/experience-class'); ?>
- <?php get_template_part('service_inc/sec-txtBocks'); ?>      
+    
  <?php endif; ?>
  <?php get_template_part('service_inc/sec-summary'); ?>
  <?php get_template_part('service_inc/service_access'); ?>
