fe3cb66e15a9e4efdd57774eabb6c83b45cf6701
神園巧真 <>
2023-07-18 22:40:27 +0900

    お問い合わせセクションのページリンクを設定

--- shichifukujin-theme/include/contact.php
+++ shichifukujin-theme/include/contact.php
@@ -1,18 +1,13 @@
 <section class="contact">
-
-
-
     <div class="contact__inner">
     <div class="contact__texts">
         <h2 class="contact__title">お問い合わせ</h2>
         <p>ご入居・見学希望は下記からお気軽にお問い合わせください。</p>
         <div class="contact__btn">
-        <a class="btn shadow02" href="">
+        <a class="btn shadow02" href="<?= home_url('/contact/') ?>">
         <i class="fa fa-envelope"></i>
         お問い合わせフォーム</a>
         </div> 
-
    </div>
     </div>
-
 </section>
