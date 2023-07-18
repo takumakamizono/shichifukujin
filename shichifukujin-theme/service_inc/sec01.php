22fca07cc95c9c9b65039c76c298e684fb01a403
神園巧真 <>
2023-07-18 23:58:47 +0900

    施設紹介ページのトップの画像とテキストのセクションのスタイル修正

--- shichifukujin-theme/service_inc/sec01.php
+++ shichifukujin-theme/service_inc/sec01.php
@@ -15,10 +15,12 @@
                         </div>  
                       </div>
                       <div class="sl-service__sec01-img">
+                        <div class="sl-service__sec01-img-inner">
                       <?php if(has_post_thumbnail()): ?>
                      <?php the_post_thumbnail('full'); ?>    
                      <?php else: ?>       
-                      <img src="https://placehold.jp/340x230.png" alt=""> 
+                      <img src="<?php echo get_template_directory_uri(); ?>/images/cover_image.png" alt=""> 
                       <?php endif; ?>
+                      </div>
                       </div>
                     </div>
