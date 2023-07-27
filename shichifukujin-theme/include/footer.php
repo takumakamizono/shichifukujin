0ccdb984ccb9bc597da1183b5256742c51c45b3c
神園巧真 <>
2023-07-27 10:36:12 +0900

    フッターのsdgsやくるみんバナーのスタイル修正

--- shichifukujin-theme/include/footer.php
+++ shichifukujin-theme/include/footer.php
@@ -9,7 +9,7 @@
             </div>
             <div class="logo__tit">
               <span>社会福祉法人</span>
-           <span>天佑会</span> 
+           <span>天祐会</span> 
            </div>
            </a>
             </div>
@@ -18,21 +18,39 @@
               <li>TEL : 099-256-2729</li>
               <li>FAX : 099-256-3729</li>
               <li>
-                <a href="<?php the_field('insta01',1241); ?>" class="footer__address-icon"><i class="fab fa-instagram-square"></i></a> <a href="<?php the_field('facebook',1241); ?>" class="footer__address-icon"> <i class="fab fa-facebook-square"></i></a></li>
-              <li></li>
+                <a href="<?php the_field('insta01',1241); ?>"target="_blank" rel="noopener" class="footer__address-icon"><i class="fab fa-instagram-square"></i></a> 
+                <a href="<?php the_field('facebook',1241); ?>"target="_blank" rel="noopener" class="footer__address-icon"> <i class="fab fa-facebook-square"></i></a>
+              </li>
             </ul>    
             </div>
             <div class="footer__right">
-              <div class="footer__right-imgs">
-            <div class="footer__img">
-              <a href="<?php the_field('sdgs',1241); ?>"target="_blank" rel="noopener">
+              <div class="footer__right-links">
+            <div class="footer__sdgs">
+            <a href="<?php the_field('sdgs',1241); ?>"target="_blank" rel="noopener">
+              <div class="footer__link-txts">
+                <p class="footer__link-tit">社会福祉法人天祐会ＳＤＧｓ宣言</p>
+                <div class="footer__link-desc">
+                当法人は、「持続可能な開発目標（ＳＤＧｓ）」に賛同しています。
+                </div>
+              </div>
+              <div class="footer__link-img">
               <img src="<?= get_template_directory_uri(); ?>/images/sdgs.png" alt="">
+              </div>
               </a>
             </div>
-            <div class="footer__img">
+            <div class="footer__kurumin">
               <a href="<?php the_field('kurumin',1241); ?>"target="_blank" rel="noopener">
+              <div class="footer__link-txts">
+                <p class="footer__link-tit">くるみんマークとは</p>
+                <div class="footer__link-desc">
+                「子育てサポート企業」として、厚生労働大臣の認定を受けた証です。
+                </div>
+              </div>
+              <div class="footer__link-img">
               <img src="<?= get_template_directory_uri(); ?>/images/kurumin.png" alt="">
+              </div>
               </a>
+             
             </div>
             </div>
           </div> 
