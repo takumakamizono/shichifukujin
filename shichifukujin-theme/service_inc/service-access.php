aa835195989ea73c1768d0443381571da094277c
神園巧真 <>
2023-07-27 10:14:50 +0900

    map apiの表示設定

--- shichifukujin-theme/service_inc/service-access.php
+++ shichifukujin-theme/service_inc/service-access.php
@@ -3,7 +3,12 @@
     <div class="sl-service__access-map">
     <!-- <iframe src="<?php echo esc_url(get_field('googlemap')); ?>"></iframe> -->
     <!-- <div id="map_canvas"></div> -->
-    <?php echo do_shortcode('[custom_map id="map1" lat="31.590504" lng="130.542486" zoom="18" title="マップ1"]'); ?>
+    <?php
+    $lat_value = get_field('lat');
+    $lng_value = get_field('lng'); 
+    
+    ?>
+    <?= do_shortcode("[custom_map id='map1' lat='{$lat_value}' lng='{$lng_value}']"); ?>
     
     
     </div>
