76fe7bd86bbcf9ab3f23642a997960b47a22f56e
神園巧真 <>
2023-10-20 13:48:01 +0900

    不要なファイルを削除

---
+++ shichifukujin-theme/service_inc/_sec02.php
@@ -0,0 +1,120 @@
+<div class="texts-blocks">
+                    <div class="texts-blocks__inner">
+                      <div class="text-box">
+                      <h4 class="text-box__title">
+                        <span>ユニットケアとは？</span>
+                     </h4>
+                     <div class="text-box__desc">
+                      <p>ユニットケアとは、ご自宅での生活環境にできる限り近づけた生活を送ることです。他の入居者様や介護スタッフと共同生活をしながら、入居者様一人ひとりの個性や生活リズムに応じて暮らしていけるようにサポートすることをいいます。</p>
+                      <p>通常、入居者様個人のプライバシーが守られる「個室」と、他の入居者様や介護スタッフと交流するための「居間」（共同生活室）があり、入居者様10人前後を一つの「ユニット」として位置づけ、各ユニットごとに顔なじみの介護スタッフが、入居者様の個性や生活リズムを尊重した暮らしをサポートするものです。</p>
+                     </div>
+                      </div>
+                      <div class="text-box">
+                      <h4 class="text-box__title">
+                         <span>わたしたちが目指すもの</span>                    
+                     </h4>
+                     <div class="text-box__desc">
+                      <p>私たちが目指すのは、「お引越しする」という、入居前と同じ生活を送る感覚。
+                         今までの暮らしと同じように暮らすことを目的としているので、ただ、七福神にお引越しするだけのことです。
+                         １０人は家族のようにリビングで過ごし、休みたい時には自分の部屋に戻ります。</p>
+                      <p>施設の作成したタイムスケジュールに入居者様を合わせるのではなく、私たちが、入居者様のお一人お一人の生活リズムに合わせてケアをします。</p>
+                      <p>その人らしい暮らしをつくっていくことが私たちの目指すユニットケアです。</p>
+                     </div>
+                      </div>
+                      <?php $post_slug = get_post_field('post_name', get_post()); ?>
+                   <?php if($post_slug === 'silkroad'): ?>
+                      <div class="text-box">
+                      <h4 class="text-box__title">
+                        <span>サービス紹介</span>    
+                     </h4>
+                     <div class="text-box__desc">
+                     
+                   
+                      <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">生活リハビリ</h5>
+                        <div class="text-box__innerDesc">
+                          <p>できる限り自分で身の回りの事ができるように、機能訓練指導員が生活に必要な訓練を行い、その方の「できたらいいなぁ」 を支援しています。</p>
+                        </div>
+                        </div>
+                      <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">アニマルセラピー</h5>
+                        <div class="text-box__innerDesc">
+                          <p>動物の力を借りて、心理的に安心を与え精神的な健康を回復させる効果があります。犬好きにはたまりません。</p>
+                        </div>
+                        </div>
+                      <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">薬膳料理</h5>
+                        <div class="text-box__innerDesc">
+                          <p>「未病」だからこそ、 体内を整える為に薬膳メニュー、薬膳スープ、 薬膳茶を取り入れ健康食に力を入れています。油は元気になるココナッツオイルを使用しています。
+</p>
+                        </div>
+                        </div>
+                      <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">眠りスキャン</h5>
+                        <div class="text-box__innerDesc">
+                          <p>入居者様の睡眠状態や心拍数や呼吸数などを把握するシステムを導入しています。入居者様の急変にも、いち早く対応することができます。</p>
+                        </div>
+                        </div>
+                   
+                     
+                      <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">看取り（施設で最期の日まで）</h5>
+                        <div class="text-box__innerDesc">
+                          <p>最期のその日まで自分らしく暮らしていただけるように、 そして、望む最期を迎えられるように、看取りケアを大切にケアいたします。</p>
+                        </div>
+                        </div>
+
+                     </div>
+                     </div>
+
+                     
+
+                     <?php elseif($post_slug === 'tokuyou'): ?>
+
+
+                     <div class="text-box">
+                     <h4 class="text-box__title">
+                        <span>サービス紹介</span>    
+                     </h4>
+                     <div class="text-box__desc">
+                     <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">生活リハビリ(心身機能活性運動療法)</h5>
+                        <div class="text-box__innerDesc">
+                          <p>認知症改善を目的に開発された5つのプログラムを実践し、 ｢脳と心と体」を楽しく活性化させ、生活にハリをもたらします。</p>
+                        </div>
+                        </div>
+                        <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">アニマルセラピー</h5>
+                        <div class="text-box__innerDesc">
+                          <p>かわいいワンちゃんが住んでいます。 毎日元気に走り回っていて、犬が大好きな利用者様と一緒に眠ったりも。入居前に飼っていたペットも一緒に入居できます。</p>
+                        </div>
+                        </div>
+                      <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">アロマテラビー</h5>
+                        <div class="text-box__innerDesc">
+                          <p>アロマの香りでリラックスできる空間づくりをしています。そして、落ち着きを取り戻し安心していただけるようにリラックス効果のあるソフトなアロママッサージを行っています。</p>
+                        </div>
+                        </div>
+                    
+                      <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">入居中の様子が分かるシステム</h5>
+                        <div class="text-box__innerDesc">
+                          <p>遠く離れたご家族様に向けた、入居者様の様子をホームページの専用ページから閲覧できるシステムがあります。ご希望でそれぞれ閲覧に暗証番号があるので安心です。</p>
+                        </div>
+                        </div>
+                        <div class="text-box__innerBox">
+                        <h5 class="text-box__innerTitle">薬膳料理</h5>
+                        <div class="text-box__innerDesc">
+                          <p>「未病」だからこそ、体内を整える為に薬膳メニュー、薬膳スープ、薬膳茶を取り入れ健康食に力を入れています。油は元気になるココナッツオイルを使用しています。</p>
+                        </div>
+                        </div>
+                     
+
+                     </div>
+                     </div>
+                     <?php endif; ?>
+                      </div>
+                    </div>
+
+
+                  
