cf394d3cacc16850300667bf95881bd594bd17d0
神園巧真 <>
2023-10-20 13:50:08 +0900

    カスタムフィールドのテキストコンテンツ表示のスタイル修正

--- shichifukujin-theme/service_inc/text-box.php
+++ shichifukujin-theme/service_inc/text-box.php
@@ -1,18 +1,31 @@
-<div class="text-box">   
-                      <h4 class="text-box__title">
+<?php if(get_field('text-title01')): ?>
+  <div class="text-box">
+<div class="text-box__wrap">   
+<h4 class="text-box__title">
                         <span><?php the_field('text-title01');?></span>
                      </h4>
-                     <div class="text-box__desc">
+ <div class="text-box__inner">
+         <div class="text-box__desc">
                       <?php the_field('text-desc01');?>
                      </div>
                 </div>
+                </div>
                 <?php if(get_field('text-title02')):?>       
-<div class="text-box">
+<div class="text-box__wrap">
                       <h4 class="text-box__title">
                         <span><?php the_field('text-title02');?></span>
                      </h4>
+                     <div class="text-box__inner">
                      <div class="text-box__desc">
                       <?php the_field('text-desc02');?>
                      </div>    
+                     
+                    </div>
+                    </div>
+              
+               
+               
+                <?php endif; ?>
+                
                 </div>
                 <?php endif; ?>
