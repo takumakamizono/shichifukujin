6c61c7f2d07d7c8a0c8e2e48af2d86c012a50eac
神園巧真 <>
2023-10-20 14:04:06 +0900

    カスタムフィールドの5つの取り組みコンテンツをサービス紹介に変更して統一しました

--- shichifukujin-theme/service_inc/sec-try.php
+++ shichifukujin-theme/service_inc/sec-try.php
@@ -1,85 +1,64 @@
-<?php if( get_field('try-quantity')): ?>
- <div class="try">
- <div class="section-titles">
-   <h3 class="main-title"><span><?php the_field('try-quantity'); ?>つの取り組み</span></h3>            
- </div>
-    <div class="try__inner">
-        <div class="try__block">
-            <div class="try__img">
-                <div class="try__img-inner ">
-                    <div class="try__circle bgBlue"></div>
-                <span> <?php the_field('try-label01'); ?></span>
-                </div>
-            </div>
-            <div class="try__txt">
-                <h5><?php the_field('try-title01'); ?></h5>
-                <p><?php the_field('try-desc01'); ?></p>
-            </div>
-        </div>
-        <div class="try__block">
-            <div class="try__img">
-                <div class="try__img-inner">
-                <div class="try__circle bgOrange"></div>
-                <span><?php the_field('try-label02'); ?></span>
-                </div>
-            </div>
-            <div class="try__txt">
-                <h5><?php the_field('try-title02'); ?></h5>
-                <p><?php the_field('try-desc02'); ?></p>
-            </div>
-        </div>
-        <div class="try__block">
-            <div class="try__img">
-                <div class="try__img-inner">
-                <div class="try__circle bgGreen"></div>
-                <span><?php the_field('try-label03'); ?></span>
-                </div>
-            </div>
-            <div class="try__txt">
-                <h5><?php the_field('try-title03'); ?></h5>
-                <p><?php the_field('try-desc03'); ?></p>
-            </div>
-        </div>
-        <div class="try__block">
-            <div class="try__img">
-                <div class="try__img-inner">
-                <div class="try__circle bgYellow"></div>
-                <span><?php the_field('try-label04'); ?></span>
-                </div>
-            </div>
-            <div class="try__txt">
-                <h5><?php the_field('try-title04'); ?></h5>
-                <p><?php the_field('try-desc04'); ?></p>
-            </div>
-        </div>
-        <div class="try__block">
-            <div class="try__img">
-                <div class="try__img-inner">
-                <div class="try__circle bgPink"></div>
-                <span><?php the_field('try-label05'); ?></span>
-                </div>
-            </div>
-            <div class="try__txt">
-                <h5><?php the_field('try-title05'); ?></h5>
-                <p><?php the_field('try-desc05'); ?></p>
-            </div>
-        </div>
-        <?php if(get_field('try-label06')): ?>
-        <div class="try__block">
-            <div class="try__img">
-                <div class="try__img-inner">
-                <div class="try__circle bgPink"></div>
-                <span><?php the_field('try-label06'); ?></span>
-                </div>
-            </div>
-            <div class="try__txt">
-                <h5><?php the_field('try-title06'); ?></h5>
-                <p><?php the_field('try-desc06'); ?></p>
-            </div>
-        </div>
-        <?php endif; ?>
+<?php if( get_field('try-title01')): ?>
+   
+                  
+               
+                      <div class="service-list">
+                        <div class="service-list__inner">
+                      <h4 class="service-list__title">
+                        <span><?php the_field('try-sectit'); ?></span>    
+                     </h4>
+                     <div class="service-list__desc">
+                     
+                   
+                      <div class="service-list__innerBox">
+                        <h5 class="service-list__innerTitle"><?php the_field('try-title01'); ?></h5>
+                        <div class="service-list__innerDesc">
+                          <p><?php the_field('try-desc01'); ?></p>
+                        </div>
+                        </div>
+                      <div class="service-list__innerBox">
+                        <h5 class="service-list__innerTitle"><?php the_field('try-title02'); ?></h5>
+                        <div class="service-list__innerDesc">
+                          <p><?php the_field('try-desc02'); ?></p>
+                        </div>
+                        </div>
+                      <div class="service-list__innerBox">
+                        <h5 class="service-list__innerTitle"><?php the_field('try-title03'); ?></h5>
+                        <div class="service-list__innerDesc">
+                          <p><?php the_field('try-desc03'); ?></p>
+                        </div>
+                        </div>
+                      <div class="service-list__innerBox">
+                        <h5 class="service-list__innerTitle"><?php the_field('try-title04'); ?></h5>
+                        <div class="service-list__innerDesc">
+                          <p><?php the_field('try-desc04'); ?></p>
+                        </div>
+                        </div>
+                   
+                        <?php if(get_field('try-title05')): ?>
+                      <div class="service-list__innerBox">
+                        <h5 class="service-list__innerTitle"><?php the_field('try-title05'); ?></h5>
+                        <div class="service-list__innerDesc">
+                          <p><?php the_field('try-desc05'); ?></p>
+                        </div>
+                        </div>
+                        <?php endif; ?>
+                        <?php if(get_field('try-title06')): ?>
+                      <div class="service-list__innerBox">
+                        <h5 class="service-list__innerTitle"><?php the_field('try-title06'); ?></h5>
+                        <div class="service-list__innerDesc">
+                          <p><?php the_field('try-desc06'); ?></p>
+                        </div>
+                        </div>
+                        <?php endif; ?>
+                        </div>
+                     </div>
+                     </div>
 
-    </div>
+                     
 
- </div>
+                
+              
  <?php endif; ?>
+
+
