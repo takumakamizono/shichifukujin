4938557fc907fa9b458f0eecdb84ab4e6a11df6b
神園巧真 <>
2023-10-20 13:35:40 +0900

    フロントページにお困りごとマップページへのリンクコンテンツを追加

--- shichifukujin-theme/include/detailed-info.php
+++ shichifukujin-theme/include/detailed-info.php
@@ -1,7 +1,7 @@
 
 <section class="detailed-info">
-               <div class="detailed-info__inner">
-                    <div class="detailed-info__block">
+               <div class="detailed-info__inner appear up">
+                    <div class="detailed-info__block item">
                     <div class="detailed-info__tit">
                         ご利用までの流れ
                       </div>
@@ -20,7 +20,7 @@
                        </div>
 
                     </div>
-                    <div class="detailed-info__block">
+                    <div class="detailed-info__block item">
                     <div class="detailed-info__tit">
                         スタッフインタビュー
                       </div>
@@ -44,7 +44,7 @@
                        </div>
 
                     </div>
-                    <div class="detailed-info__block">
+                    <div class="detailed-info__block item">
                       <div class="detailed-info__tit">
                         よくある質問
                       </div>
@@ -58,6 +58,30 @@
                             </div>
                             <div class="detailed-info__btn">
                         <a class="btn slide-bg" href="<?= home_url('/faq/') ?>">
+                        詳細はこちら
+                       </a>
+                       </div>
+                       </div>
+
+                    </div>
+                    <div class="detailed-info__block item">
+                    <div class="detailed-info__tit">
+                        お困りごとガイドマップ
+                      </div>
+                        <div class="detailed-info__img">
+                            <img src="<?= get_template_directory_uri(); ?>/images/interview.jpg" alt="">
+                        </div>
+                        <div class="detailed-info__text">
+                            <div class="detailed-info__desc">
+                        <p>利用者様・ご家族様のお困りごとに対して、七福神がお手伝いできることをガイドマップにしました。</p>
+
+                            </div>
+                           <?php
+                            $interview  = get_term_by('slug','interview','category');
+                            $interview_link = get_term_link($interview,'category');
+                           ?>
+                    <div class="detailed-info__btn">
+                        <a class="btn slide-bg" href="<?= home_url('/guide/') ?>" >
                         詳細はこちら
                        </a>
                        </div>
