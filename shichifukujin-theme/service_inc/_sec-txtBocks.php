47a54cde5c3dffad119002b7a48cbc7db505610e
神園巧真 <>
2023-10-25 12:42:56 +0900

    不要なファイルを削除しました

--- shichifukujin-theme/service_inc/_sec-txtBocks.php
+++
@@ -1,9 +0,0 @@
-<?php $post_slug = get_post_field('post_name', get_post());?>
-   <div class="table-blocks">
-    <div class="table-blocks__inner">
-    <?php get_template_part('service_inc/text-box'); ?>
-    <?php if(!$post_slug === 'support'): ?>
-    <?php get_template_part('service_inc/image-box'); ?>   
-    <?php endif; ?>  
-</div>
-</div>
