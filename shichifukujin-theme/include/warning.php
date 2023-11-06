ec9d67be671be619f18ad2243b9da9733ccab6d4
神園巧真 <>
2023-11-06 12:56:55 +0900

    注意書きの固定ページ番号の修正

--- shichifukujin-theme/include/warning.php
+++ shichifukujin-theme/include/warning.php
@@ -1,7 +1,7 @@
 <div class="warning">
     <div class="warning__inner">
     <?php
-                  $page_id = 1692;
+                  $page_id = 1428;
                   $alternative_post = get_post( $page_id );      
                   echo apply_filters('the_content', $alternative_post->post_content); //固定ページの内容                     
                   ?>
