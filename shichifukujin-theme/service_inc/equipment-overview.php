066c626e60a5ceeae4319e1a9305ef3661dcc902
神園巧真 <>
2023-11-16 10:04:30 +0900

    特養の設備概要の表の部分のファイル名をsec03から変更 1日の流れのブロックを分離して別コンポーネントに。

--- shichifukujin-theme/service_inc/equipment-overview.php
+++ shichifukujin-theme/service_inc/equipment-overview.php
@@ -1,8 +1,5 @@
-<div class="surround-contents ">
+<section class="surround-contents ">
    <div class="surround-contents__inner">
-   
-<?php get_template_part('service_inc/facility-slide'); ?>
-
     <div class="surround-contents__box">
     <div class="section-titles">
     <h4 class="main-title"><span>設備概要</span></h4>
@@ -55,23 +52,7 @@
 </tr>
 </table>
   </div>
-  <div class="surround-contents__box">
-
-  <div class="info-with-imgs">
-    <div class="section-titles">
-    <h4 class="main-title"><span>1日の流れ</span></h4>
-  </div>
-  <div class="info-with-imgs__desc">
-    <p>今までの生活スタイルをそのまま継続します。  <br> 一人ひとり違う生活時間、皆さまの24時間を尊重します。
-起床時間や食事の時間は自由です。個別ケアで対応します。</p>
-  </div>
-  <div class="info-with-imgs__img">
-    <img src="<?= get_template_directory_uri(); ?>/images/charge-schedule.jpg" alt="">
-  </div>
-  </div>
-  </div>
-
-
-  </div> 
+  
   <div class="surround-contents__bg"></div>           
  </div>
+ </section>
---
+++ shichifukujin-theme/service_inc/flow-day.php
@@ -0,0 +1,21 @@
+<section class="surround-contents ">
+   <div class="surround-contents__inner">
+<div class="surround-contents__box">
+ 
+    <div class="section-titles">
+    <h4 class="main-title"><span>1日の流れ</span></h4>
+  </div>
+  <div class="info-with-imgs__desc">
+    <p>今までの生活スタイルをそのまま継続します。  <br> 一人ひとり違う生活時間、皆さまの24時間を尊重します。
+起床時間や食事の時間は自由です。個別ケアで対応します。</p>
+  </div>
+  <div class="info-with-imgs__img">
+    <img src="<?= get_template_directory_uri(); ?>/images/charge-schedule.jpg" alt="">
+  </div>
+  </div>
+  </div>
+  <div class="surround-contents__bg"></div>     
+</section>
+
+
+  
