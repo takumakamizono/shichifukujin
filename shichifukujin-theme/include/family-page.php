4838581d71040cd1951741735c4eb447b2616a8f
神園巧真 <>
2024-01-13 18:40:55 +0900

    トップページのご家族様セクションのテキスト修正

--- shichifukujin-theme/include/family-page.php
+++ shichifukujin-theme/include/family-page.php
@@ -2,12 +2,12 @@
   <div class="family-page__inner">
 <div class="section-titles">
                   <h2 class="main-title">
-                    <span>ご入居様ご家族ページ</span>                
+                    <span>ご家族様へ</span>                
                   </h2>         
                 </div>
        <div class="family-page__btn">
        <?php if (get_field('onoff',1241)): ?>
-        <a class="btn shadow02 btn-off" href="">ご家族ページは準備中です。</a>
+        <a class="btn shadow02 btn-off" href="">ご家族様ページは現在準備中です。</a>
         <?php  else: ?> 
         <a class="btn shadow02" href="<?php the_field('url',1241); ?>">ご家族ページはコチラから</a>
         <?php endif;?>
